3eec9ae5d2ef5e41d4b5a9ef885d03275284fac8
Henz Montera <Henz Montera@Henzz-Montera.local>
2020-10-14 00:08:36 +0800

    CLL FINAL

---
+++ src/cashiersys/Cashiersys.java
@@ -0,0 +1,37 @@
+// Author: John Henly A. Montera
+// Date: Month/day/2020
+// Project: Cashier System 
+
+package cashiersys;
+
+
+public class Cashiersys {
+    
+    regui qwerty = new regui();
+    
+    static String SFname[] = new String [500],SDname[] = new String [500];
+    static double SDprice[] = new double [500],SFprice[] = new double [500];
+
+    public static void main(String[] args) {
+        
+            
+    }
+    
+    public static void register(){
+        
+        
+        
+    }
+    
+    public static void system(){
+        
+        
+    }
+    
+    public static void output() {
+        
+        
+    }
+    
+}
+
